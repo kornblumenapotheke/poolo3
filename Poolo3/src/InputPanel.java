083edbb923fba2df3355d393a7cf7832c91a2b6a
GuntherBackoffice <GuntherBackoffice@DESKTOP-CHEF.kornblumen.xp>
2022-04-29 14:29:41 +0200

    aaa

---
+++ Poolo3/src/InputPanel.java
@@ -0,0 +1,389 @@
+//https://stackoverflow.com/questions/34770139/gridbag-layout-how-to-push-components-north
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.border.Border;
+import javax.swing.tree.DefaultTreeModel;
+
+public class InputPanel extends JPanel
+{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final JPanel rightPanel_1 = new JPanel(new GridBagLayout()) {
+
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public Dimension getPreferredSize() {
+
+            return new Dimension(1000, 550);
+        };
+    };
+    
+    private final JPanel rightPanel_2 = new JPanel(new GridBagLayout()) {
+
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public Dimension getPreferredSize() {
+
+            return new Dimension(800, 300);
+        };
+    };
+
+   
+
+    // BORDERS
+    private final Border homeTopPanel_LineBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+    private final Border rightPanel1_LineBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+    private final Border rightPanel2_LineBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+    private JTree tree;
+    private PCRTree myPCRTree;    
+    
+    GridBagConstraints gbc = new GridBagConstraints();
+    
+  
+    
+    public final void constructLeftButtons() {
+ 
+    	 gbc.weightx = 0;
+         gbc.weighty = 0;
+         gbc.gridheight = 1;
+         gbc.gridx = 0;
+         gbc.gridy = 1;
+
+         gbc.anchor = GridBagConstraints.PAGE_START;
+        JButton button3 = new JButton ("Save");
+		button3.setName("InputFrame_Save");
+		button3.addActionListener(PooloActionListener.getInstance());
+		this.add(button3, gbc);
+        gbc.anchor = GridBagConstraints.NORTH;
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+
+		JButton button4 = new JButton ("Print");
+		button4.setName("InputFrame_Save_Print");
+		button4.addActionListener(PooloActionListener.getInstance());
+		this.add(button4, gbc);
+		
+
+		gbc.gridx = 3;
+        gbc.gridy = 8;
+        gbc.ipady=2;
+        gbc.weighty=2.0;
+        textField.setPreferredSize(new Dimension (250,30));
+        this.add(textField, gbc);
+		
+    }
+    
+
+    public final void constructRightPanel_1() {
+
+        rightPanel_1.setBackground(Color.RED);
+        gbc.fill = GridBagConstraints.BOTH; 
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        this.add(rightPanel_1, gbc);
+    }
+
+    
+    public final void constructRightPanel_2() {
+    	log.info("constructRightPanel_2");
+    	
+        rightPanel_2.setBackground(Color.WHITE);
+        gbc.gridheight = 8;
+        rightPanel_1.add(rightPanel_2, gbc);
+    }
+    
+    
+    public final void constructRightPanel_2_Components()
+    {
+    	log.info("constructRightPanel_2_Components");
+    	gbc.weightx = 10;
+        gbc.weighty = 0;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+    	rightPanel_2.add(new JScrollPane(tree), gbc);  	
+    }
+    
+    
+
+    public final void constructLeftPanelComponents() {
+
+       
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        JButton button1 = new JButton ("New Rack");
+		button1.setName("InputFrame_newRack");
+		button1.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button1, gbc);        
+		
+		
+		gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        JButton button2 = new JButton ("New Row");
+		button2.setName("InputFrame_newRow");
+		button2.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button2, gbc);        
+        
+		
+		gbc.gridx = 0;
+        gbc.gridy = 2;                
+        JButton button5 = new JButton ("4er Pool");
+		button5.setName("InputFrame_Pool_4");
+		button5.setBackground(Color.yellow);
+		button5.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button5,gbc);
+		
+		gbc.gridx = 0;
+        gbc.gridy = 3;
+		JButton button6 = new JButton ("8er Pool");
+		button6.setName("InputFrame_Pool_8");
+		button6.setBackground(Color.yellow);
+		button6.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button6,gbc);
+		
+		gbc.gridx = 0;
+        gbc.gridy = 4;
+        JButton button7 = new JButton ("GRP LIN");
+		button7.setName("InputFrame_Group_linear");
+		button7.setBackground(Color.white);
+		button7.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button7,gbc);
+		
+		gbc.gridx = 0;
+        gbc.gridy = 5;
+		JButton button8 = new JButton ("GRP MTX");
+		button8.setName("InputFrame_Group_Matrix");
+		button8.setBackground(Color.white);
+		button8.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button8,gbc);
+		
+		gbc.gridx = 0;
+        gbc.gridy = 6;
+		JButton button9 = new JButton ("GRP BIN");
+		button9.setName("InputFrame_Group_binary");
+		button9.setBackground(Color.white);
+		button9.addActionListener(PooloActionListener.getInstance());
+		rightPanel_1.add(button9,gbc);        
+    }
+    
+
+    private void setRightPanelBorders() {
+
+        rightPanel_1.setBorder(rightPanel1_LineBorder);
+        rightPanel_2.setBorder(rightPanel2_LineBorder);
+        this.setBorder(homeTopPanel_LineBorder);
+    }
+    
+    
+	
+	ArrayList<Probenrack> racks = new ArrayList<Probenrack>();
+	Probenrack aktuelles_Rack = new Probenrack();
+	char actualRackNumber = 'M';
+	char actualRowNumber = 'A';
+	int actualPos = '0';
+	public static final Logger log = Logger.getLogger(InputPanel.class.getName());
+	Container c;
+	GridBagLayout gbl;
+	
+	JPanel jPanel ;	
+	JTextField textField = new JTextField ("");
+	JLabelExtended jLScanMonitor = new JLabelExtended(); // wird verwendet für das HTML, kann durch einen String ersetzt werden !
+	//JPanelExtendedNew jLScanMonitorNew = new JPanelExtendedNew (); 
+
+
+//Konstruktor
+	InputPanel ()
+	{
+		
+		log.info("Konstruktor Inputframe Beginn");
+		actualRackNumber--;
+		actualRowNumber--;
+		log.info("JPanel anlegen");
+		myPCRTree = new PCRTree();
+		tree = new JTree(myPCRTree.getTreeRoot() );
+		myPCRTree.setTree(tree);
+		tree.setCellRenderer(new MyTreeCellRenderer());
+		
+		
+		setScanLine();
+		
+		this.setLayout(new GridBagLayout()); //setting of layout should ALWAYS come first before adding constraints and components
+		constructLeftButtons();
+	    constructRightPanel_1();
+	    constructRightPanel_2();
+	    constructLeftPanelComponents();
+        constructRightPanel_2_Components();
+
+	    setRightPanelBorders();
+	    
+		
+		
+		
+		log.info("Registrierten beim Listener");
+		PooloActionListener.getInstance().registerService("InputFrame", (Object)this);
+		
+		log.info("Konstruktor Inputframe Ende");
+	}
+	
+	
+	/**
+	 * Registriere Scanzeile 
+	 */
+	private void setScanLine()
+	{
+		log.info("Textfield anlegen");
+		textField.addActionListener(PooloActionListener.getInstance());
+		textField.setName("EANScan");
+		textField.setFocusable(true);
+		textField.setSize(250, 30);
+	
+	}
+	
+	
+
+	 
+	
+	
+	
+	//was muss durchgef�hrt werdne, wenn scan durchgef�hrt wurde
+	/**public void addScan (String inString) 
+	{
+		log.info("addcan aufgerufen");		
+		jLScanMonitor.newLine(inString);
+		textField.setFocusable(true);
+	} BRAUCHT MAN DAS??**/ 
+	
+	/**
+	 * Eine neue Probe wird angelegt
+	 * @param inString Probenbezeichnung
+	 */
+	public void newProbe (String inString)
+	{
+		log.info("Neue Probe anlegen ");
+		//checke ob diese Probe nicht schon existiert (Hash).
+		//Probenobjekt anlegen mit allen Referenzen!
+		// if (insString not in Hash)
+		actualPos++;
+		textField.setText("");
+		jLScanMonitor.newLine("&emsp;<font size='3' bgcolor='white' color='black'> "+actualPos+":"+inString+"</font>");
+		aktuelles_Rack.addProbe(inString, Character.toString(actualRowNumber), Integer.toString(actualPos));	
+		myPCRTree.addProbe(Integer.toString(actualPos)+": "+inString);
+		((DefaultTreeModel)tree.getModel()).reload();
+		JTreeUtil jTreeUtil = new JTreeUtil();
+		jTreeUtil.expandTree(tree, true);
+		//endif
+		textField.grabFocus();
+	}
+	
+	/**
+	 * Ein neues Rack soll angelegt werden
+	 */
+	public void newRack ()
+	{
+		log.info("Neuen rack anlegen");		
+		actualRackNumber++;
+		actualRowNumber ='A';
+		actualRowNumber--;
+		jLScanMonitor.newLine("<font size='8' bgcolor='black' color='orange'> New Rack "+actualRackNumber+"</font>");
+		textField.grabFocus();
+		Probenrack aktuelles_Rack = new Probenrack (); //daten mit übergeben!
+		racks.add(aktuelles_Rack);
+		myPCRTree.addRack(Character.toString(actualRackNumber));
+		((DefaultTreeModel)tree.getModel()).reload();
+		JTreeUtil jTreeUtil = new JTreeUtil();
+		jTreeUtil.expandTree(tree, true);
+		textField.grabFocus();
+
+			
+		}
+	
+	/**
+	 * Eine neue Linie wird angelegt
+	 */
+	public void newRow ()
+	{
+		log.info("Neue Row anlegen");
+		//neue Row anlegen
+		//text ausgeben
+		actualRowNumber++;
+		actualPos=0;
+		jLScanMonitor.newLine("&ensp;<font size='5' bgcolor='black' color='green'> New ROW " + actualRowNumber + "("+(Character.getNumericValue(actualRowNumber)-9)+")"+"</font>");
+		textField.grabFocus();
+		aktuelles_Rack.addRow(Character.toString(actualRowNumber)); //Daten übergeben
+		myPCRTree.addLine(Character.toString(actualRowNumber));
+		((DefaultTreeModel)tree.getModel()).reload();
+		JTreeUtil jTreeUtil = new JTreeUtil();
+		jTreeUtil.expandTree(tree, true);
+		textField.grabFocus();
+	}
+	
+	/**
+	 * Als HTMl abspeichern
+	 */
+	public void save_to_file()
+	{
+		log.info("Save to file ausgel�st");
+		HTMLPrinter.getInstance().save_to_file(jLScanMonitor.getText());
+	}
+	
+	
+	
+	public static void main (String args[])
+	{
+		
+		JFrame frame = new JFrame();
+        frame.add(new InputPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+		
+		
+		
+	}
+
+/**
+ * Ausdruck als HTML via Browser
+ */
+	public void print() {
+		
+		log.info("Save to file und drucken ausgel�st");
+		HTMLPrinter.getInstance().print(jLScanMonitor.getText());	
+	
+	}
+
+	
+	
+}
