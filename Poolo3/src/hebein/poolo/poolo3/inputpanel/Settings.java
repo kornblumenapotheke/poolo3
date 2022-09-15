7248578def1c548fef089c14bb8dab06c708b88f
kornblumenapotheke <office@meineapo.at>
2022-09-15 21:05:59 +0200

    Versionsnummer angepasst Farbe Probe grüna uf weiss besser lesbar ROW heisst jetzt Line beim Button

--- Poolo3/src/hebein/poolo/poolo3/inputpanel/Settings.java
+++ Poolo3/src/hebein/poolo/poolo3/inputpanel/Settings.java
@@ -4,14 +4,14 @@ import java.awt.Color;
 
 public class Settings {
 
-	public static final String version = "0.99.01a // 24.7.2022";
+	public static final String version = "0.99.02 // 15.09.2022";
 	public static final String copyright = "(C) Gunther Hebein 2022";
 	public static final Color foreground_einzelprobe_INVALID = Color.red;
 	public static final Color background_einzelprobe_INVALID = Color.black;
 	public static final Color foreground_einzelprobe_NOT_CONSTRUCTED = Color.red;
 	public static final Color background_einzelprobe_NOT_CONSTRUCTED = Color.black;
 	public static final Color foreground_einzelprobe_IST_IN_AUSWERTUNG = Color.GREEN;
-	public static final Color background_einzelprobe_IST_IN_AUSWERTUNG = Color.LIGHT_GRAY;
+	public static final Color background_einzelprobe_IST_IN_AUSWERTUNG = Color.WHITE;
 	public static final Color foreground_einzelprobe_IST_POSITIV = Color.black;
 	public static final Color background_einzelprobe_IST_POSITIV = Color.red;
 	public static final Color foreground_einzelprobe_IST_NEGATIV = Color.black;
